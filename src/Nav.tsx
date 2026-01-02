4523f1ea8765fccf51f9d67c7d62f3d6e3927192
DU <sangdyjjang@naver.com>
2026-01-02 16:44:14 +0900

    feat: Nav 컴포넌트에 현재 페이지 표시 기능 추가 및 스타일 개선

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,19 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Nav.css";
 
 function Nav() {
+  const location = useLocation(); // 현재 페이지 표시를 위해 추가
+
   return (
-    <div className="navbar">
-      <Link to="/" className="navbarTitle">
-        <h1>Nobel Winners</h1>
-      </Link>
-      <Link className="navbarMenu" to="/about">
-        About
-      </Link>
-      <Link className="navbarMenu" to="/contact">
-        Contact
-      </Link>
-    </div>
+    <nav className="apple-navbar">
+      <div className="navbar-content">
+        <Link to="/" className="navbar-logo">
+          Nobel Explorer
+        </Link>
+
+        <div className="navbar-links">
+          <Link
+            className={`navbar-link ${
+              location.pathname === "/about" ? "active" : ""
+            }`}
+            to="/about"
+          >
+            About
+          </Link>
+          <Link
+            className={`navbar-link ${
+              location.pathname === "/contact" ? "active" : ""
+            }`}
+            to="/contact"
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
   );
 }
 
